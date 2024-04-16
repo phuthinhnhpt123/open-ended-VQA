95dfb8b5920d20cde73a89e8325b17501644c0ae
edgar-nbc <2512@newbitcoincity.com>
2024-04-16 16:28:05 +0700

    update read

--- utils.py
+++ utils.py
@@ -72,6 +72,6 @@ def generate_beam(
         for output, length in zip(output_list, seq_lengths)
     ]
     order = scores.argsort(descending=True)
-    print(order)
+    print(output_texts)
     output_texts = [output_texts[i] for i in order]
     return output_texts
