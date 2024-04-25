3f7578bbd5ace50a7d3ac216698949b9ada60c02
phuthinhnhpt123 <nguyenhuynhphuthinh123@gmail.com>
2024-04-25 09:30:39 +0700

    add

--- read.py
+++ read.py
@@ -11,98 +11,113 @@ from torch.cuda.amp import autocast
 from utils import generate_beam
 import os
 
-def set_random_seeds(random_seed=0):
+# def set_random_seeds(random_seed=0):
 
-    torch.manual_seed(random_seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    np.random.seed(random_seed)
-    random.seed(random_seed)
+#     torch.manual_seed(random_seed)
+#     torch.backends.cudnn.deterministic = True
+#     torch.backends.cudnn.benchmark = False
+#     np.random.seed(random_seed)
+#     random.seed(random_seed)
 
-def parse_argument():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model_type", type=str, default="gpt2-xl")
-    parser.add_argument("--setting", type=str, default="lora", choices=("lora", "frozen"))
-    parser.add_argument("--mapping_type", type=str, default="MLP")
-    parser.add_argument("--prefix_length", type=int, default=8)
-    parser.add_argument(
-        "--dataset_path", type=str, default="../visual7w/"
-    )
-    parser.add_argument("--batch_size", type=int, default=32)
-    parser.add_argument("--epochs", type=int, default=30)
-    parser.add_argument("--lr", type=float, default=1e-4)
-    parser.add_argument("--warmup_steps", type=int, default=600)
-    parser.add_argument("--seed", type=int, default=0)
-    parser.add_argument("--iters_to_accumulate", type=int, default=4)
-    parser.add_argument("--validation_step", type=int, default=1000)
-    parser.add_argument("--out_dir", default="./checkpoints")
-    parser.add_argument("--checkpoint", type=str)
-    parser.add_argument("--eval", dest="eval", action="store_true")
+# def parse_argument():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("--model_type", type=str, default="gpt2-xl")
+#     parser.add_argument("--setting", type=str, default="lora", choices=("lora", "frozen"))
+#     parser.add_argument("--mapping_type", type=str, default="MLP")
+#     parser.add_argument("--prefix_length", type=int, default=8)
+#     parser.add_argument(
+#         "--dataset_path", type=str, default="../visual7w/"
+#     )
+#     parser.add_argument("--batch_size", type=int, default=32)
+#     parser.add_argument("--epochs", type=int, default=30)
+#     parser.add_argument("--lr", type=float, default=1e-4)
+#     parser.add_argument("--warmup_steps", type=int, default=600)
+#     parser.add_argument("--seed", type=int, default=0)
+#     parser.add_argument("--iters_to_accumulate", type=int, default=4)
+#     parser.add_argument("--validation_step", type=int, default=1000)
+#     parser.add_argument("--out_dir", default="./checkpoints")
+#     parser.add_argument("--checkpoint", type=str)
+#     parser.add_argument("--eval", dest="eval", action="store_true")
 
-    parser.add_argument("--verbose", dest="verbose", action="store_true")
+#     parser.add_argument("--verbose", dest="verbose", action="store_true")
 
-    args = parser.parse_args()
+#     args = parser.parse_args()
     
-    set_random_seeds(args.seed)
-    return args
+#     set_random_seeds(args.seed)
+#     return args
 
-def print_nearest_text_token(vis_token, model):
-    """print the nearest token in the vocabulary to the given token through model.gpt.embeddings.weight"""
-    embeddings = model.gpt.transformer.wte.weight
-    distances = torch.norm(embeddings - vis_token, dim=1)
-    nearest_token_idx = torch.argmin(distances)
-    print(model.tokenizer.decode([nearest_token_idx.item()])) 
+# def print_nearest_text_token(vis_token, model):
+#     """print the nearest token in the vocabulary to the given token through model.gpt.embeddings.weight"""
+#     embeddings = model.gpt.transformer.wte.weight
+#     distances = torch.norm(embeddings - vis_token, dim=1)
+#     nearest_token_idx = torch.argmin(distances)
+#     print(model.tokenizer.decode([nearest_token_idx.item()])) 
 
-if __name__ == "__main__":
-    args = parse_argument()
-    suffix = f"v5_prefixlength_{args.prefix_length}_seed_{args.seed}_gpttype_{args.model_type.replace('/','')}_setting_{args.setting}"
+# if __name__ == "__main__":
+#     args = parse_argument()
+#     suffix = f"v5_prefixlength_{args.prefix_length}_seed_{args.seed}_gpttype_{args.model_type.replace('/','')}_setting_{args.setting}"
 
-    args.out_dir = os.path.join('../checkpoints', suffix)
-    train_dataset = VqaDataset(args.dataset_path+'/',split="train",prefix_length=args.prefix_length,model_type=args.model_type)
-    val_dataset = VqaDataset(args.dataset_path+'/',split="val",prefix_length=args.prefix_length,model_type=args.model_type)
-    test_dataset = VqaDataset(args.dataset_path+'/',split="test",prefix_length=args.prefix_length,model_type=args.model_type,like_test=True)
+#     args.out_dir = os.path.join('../checkpoints', suffix)
+#     train_dataset = VqaDataset(args.dataset_path+'/',split="train",prefix_length=args.prefix_length,model_type=args.model_type)
+#     val_dataset = VqaDataset(args.dataset_path+'/',split="val",prefix_length=args.prefix_length,model_type=args.model_type)
+#     test_dataset = VqaDataset(args.dataset_path+'/',split="test",prefix_length=args.prefix_length,model_type=args.model_type,like_test=True)
 
-    model = VQAModel(
-        prefix_length=args.prefix_length,
-        clip_length=4,
-        setting=args.setting,
-        mapping_type=args.mapping_type,
-        args=args,
-    )
+#     model = VQAModel(
+#         prefix_length=args.prefix_length,
+#         clip_length=4,
+#         setting=args.setting,
+#         mapping_type=args.mapping_type,
+#         args=args,
+#     )
 
-    train_dataloader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, shuffle=True, drop_last=True)
-    val_dataloader = DataLoader(dataset=val_dataset, batch_size=args.batch_size, shuffle=False, drop_last=True)
-    test_dataloader = DataLoader(dataset=test_dataset, batch_size=args.batch_size, shuffle=False, drop_last=True)
+#     train_dataloader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, shuffle=True, drop_last=True)
+#     val_dataloader = DataLoader(dataset=val_dataset, batch_size=args.batch_size, shuffle=False, drop_last=True)
+#     test_dataloader = DataLoader(dataset=test_dataset, batch_size=args.batch_size, shuffle=False, drop_last=True)
 
-
-    if not args.eval:
-        model = pytorch_model_run(train_dataloader, val_dataloader, model, args)
-    else:
-        checkpoint = os.path.join(args.out_dir, f"open_ended_latest.pt")
-        if args.verbose:
-            print(f">> Loading pre-trained model {checkpoint}!")
-        if os.path.exists(checkpoint):
-            model.load_state_dict(
-                torch.load(checkpoint, map_location=torch.device("cpu")), strict=False
-            )
-        else:
-            raise ValueError("Please provide valid path for loading checkpoint")
+#     print(len(train_dataset['questions']))
+    # if not args.eval:
+    #     model = pytorch_model_run(train_dataloader, val_dataloader, model, args)
+    # else:
+    #     checkpoint = os.path.join(args.out_dir, f"open_ended_latest.pt")
+    #     if args.verbose:
+    #         print(f">> Loading pre-trained model {checkpoint}!")
+    #     if os.path.exists(checkpoint):
+    #         model.load_state_dict(
+    #             torch.load(checkpoint, map_location=torch.device("cpu")), strict=False
+    #         )
+    #     else:
+    #         raise ValueError("Please provide valid path for loading checkpoint")
         
-        print_vis_token_meaning = True
-        for item in range(0,10):
-            prefix,  labels, tokens, mask, q_len = test_dataset[item]
-            prefix = prefix.type(torch.float32)
-            tokens = tokens.type(torch.long)
-            mask = mask.type(torch.long)
-            with autocast(dtype=torch.float16):
-                with torch.no_grad():
-                    embed = model.generate(prefix,labels,tokens,mask,q_len).view(1,tokens.size(0),-1)
-                    if print_vis_token_meaning:
-                        prefix_projections = embed[:,q_len:q_len+model.prefix_length,:]
-                        for i in range(prefix_projections.size(1)):
-                          print_nearest_text_token(prefix_projections[0,i], model)
-                    out_text = generate_beam(model, model.tokenizer,generated=embed,entry_length=test_dataset.max_seqs_len[1], temperature=1)[0]
-                    print(out_text)
-                    print(test_dataset.answers[item])
+    #     print_vis_token_meaning = True
+    #     for item in range(0,10):
+    #         prefix,  labels, tokens, mask, q_len = test_dataset[item]
+    #         prefix = prefix.type(torch.float32)
+    #         tokens = tokens.type(torch.long)
+    #         mask = mask.type(torch.long)
+    #         with autocast(dtype=torch.float16):
+    #             with torch.no_grad():
+    #                 embed = model.generate(prefix,labels,tokens,mask,q_len).view(1,tokens.size(0),-1)
+    #                 if print_vis_token_meaning:
+    #                     prefix_projections = embed[:,q_len:q_len+model.prefix_length,:]
+    #                     for i in range(prefix_projections.size(1)):
+    #                       print_nearest_text_token(prefix_projections[0,i], model)
+    #                 out_text = generate_beam(model, model.tokenizer,generated=embed,entry_length=test_dataset.max_seqs_len[1], temperature=1)[0]
+    #                 print(out_text)
+    #                 print(test_dataset.answers[item])
 
-   
+
+import pickle 
+
+dataset_path = "visual7w_data"
+with open(dataset_path + '/train.pkl', 'rb') as f:
+    train_data = pickle.load(f)
+with open(dataset_path + '/test.pkl', 'rb') as f:
+    test_data = pickle.load(f)
+with open(dataset_path + '/val.pkl', 'rb') as f:
+    val_data = pickle.load(f)
+
+print(len(train_data['questions']))
+print(len(test_data['questions']))
+print(len(val_data['questions']))
+
+
--- test.py
+++ test.py
@@ -9,10 +9,17 @@ import numpy as np
 import random
 import os
 import pandas as pd
+import pickle
 from datasets import load_dataset, Dataset, DatasetDict
 from collections import defaultdict
 
 data_dir = "visual7w_data"
+
+def count_questions(dataset):
+    count = 0
+    for item in dataset:
+        count = count + len(item['qa_pairs'])
+    return count
 
 def split_dataset(data_dir):
     data = load_dataset("json", data_files=os.path.join(data_dir,"annotations", "dataset_v7w_telling.json"), field="images", split="train")
@@ -29,10 +36,13 @@ def split_dataset(data_dir):
 
     return dataset
 
-data = split_dataset(data_dir)
+def read_pickle(data_dir,split):
+    data_path = data_dir +'/' +split +'.pkl'
+    with open(data_path, 'rb') as f:
+        data = pickle.load(f)
+    return data
 
 def stats_question_type(data, split):
-    
     questions = []
     answers = []
     types = []
@@ -46,24 +56,116 @@ def stats_question_type(data, split):
     
     question_type_dict = {"questions": questions, "answers": answers, "types": types}
 
-    return question_type_dict
+    return pd.DataFrame(question_type_dict)
+
+def sample_dataset(df):
+    df_who = df[df['types'] == 'who']
+    df_why = df[df['types'] == 'why']
+    df_when = df[df['types'] == 'when']
+
+    sample_who = df_who.sample(n=100)
+    sample_why = df_why.sample(n=100)
+    sample_when = df_when.sample(n=100)
+
+    sample = pd.concat([sample_who,sample_why,sample_when]).reset_index(drop=True)
+
+    sentences={"q_a":[]}
+    for i in range(len(sample)):
+        sentence=''
+        sentence = sentence + 'q: ' + sample['questions'][i] + '. a: ' + sample['answers'][i]
+        sentences['q_a'].append(sentence)
+    d=pd.DataFrame(data=sentences)
+
+    d.to_csv("sample.csv",index=False)
+    print("Saved csv file")
 
 
-a = stats_question_type(data,'train')
-df = pd.DataFrame(a)
-type_counts = df['types'].value_counts()
-print(type_counts)
+data = split_dataset(data_dir)
+# df = stats_question_type(data,'train')
+# type_counts = df['types'].value_counts()
+# print(type_counts)
 
-# df_why = df[df['types'] == 'why']
-# print(df_why.head(10))
+# sample_dataset(df)
 
-df_who = df[df['types'] == 'who']
-df_why = df[df['types'] == 'why']
-df_when = df[df['types'] == 'when']
 
-sample_who = df_who.sample(n=100).reset_index(drop=True)
-sample_why = df_why.sample(n=100).reset_index(drop=True)
-sample_when = df_when.sample(n=100).reset_index(drop=True)
+# train_data = read_pickle(data_dir,'train')
+# test_data = read_pickle(data_dir,'test')
+# val_data = read_pickle(data_dir,'val')
 
-sample = pd.concat([sample_who,sample_why,sample_when])
-print(sample)
+# print("num train data: ", len(train_data['questions']))
+# print("num test data: ", len(test_data['questions']))
+# print("num val data: ", len(val_data['questions']))
+
+def get_question_types(dataset):
+    question_types = {}
+    for item in dataset:
+        for qa in item['qa_pairs']:
+            q_type = qa['type']
+            qa_id = qa['qa_id']
+            if q_type not in question_types:
+                question_types[q_type] = set()
+            question_types[q_type].add(qa_id)
+    return {key: list(values) for key, values in question_types.items()}
+
+def stratified_sample(dataset, question_types, ratio=1/6):
+    subset_ids = set()
+    for q_type, ids in question_types.items():
+        num_samples = int(len(ids) * ratio)
+        chosen_ids = np.random.choice(ids, num_samples, replace=False)
+        subset_ids.update(chosen_ids)
+
+    new_items = []
+    for item in dataset:
+        filtered_qa_pairs = [qa for qa in item['qa_pairs'] if qa['qa_id'] in subset_ids]
+        if filtered_qa_pairs:
+            new_item = item.copy()
+            new_item['qa_pairs'] = filtered_qa_pairs
+            new_items.append(new_item)
+    
+    return new_items
+
+def reformat_data(filtered_data):
+    # Khởi tạo một dictionary với keys là các fields và values là empty lists
+    reformatted_data = {key: [] for key in filtered_data[0].keys()}
+    for item in filtered_data:
+        for key in item:
+            reformatted_data[key].append(item[key])
+    return reformatted_data
+
+print("num train bf: ", count_questions(data['train']))
+print("num test bf: ", count_questions(data['test']))
+print("num val bf: ", count_questions(data['val']))
+
+question_types_train = get_question_types(data['train'])
+train_filtered = stratified_sample(data['train'], question_types_train)
+
+question_types_test = get_question_types(data['test'])
+test_filtered = stratified_sample(data['test'], question_types_test)
+
+question_types_val = get_question_types(data['val'])
+val_filtered = stratified_sample(data['val'], question_types_val)
+
+train_data_dict = reformat_data(train_filtered)
+test_data_dict = reformat_data(test_filtered)
+val_data_dict = reformat_data(val_filtered)
+
+# # Tạo các Dataset từ dữ liệu đã lọc
+train_dataset = Dataset.from_dict(train_data_dict)
+test_dataset = Dataset.from_dict(test_data_dict)
+val_dataset = Dataset.from_dict(val_data_dict)
+
+print("num train af: ", count_questions(train_dataset))
+print("num test af: ", count_questions(test_dataset))
+print("num val af: ", count_questions(val_dataset))
+# Tạo DatasetDict mới
+# new_dataset = DatasetDict({
+#     'train': train_dataset,
+#     'test': test_dataset,
+#     'val': val_dataset
+# })
+
+# print(new_dataset)
+
+
+
+
